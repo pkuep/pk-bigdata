25f2da90557ef7b7e28a3418f3bcb6c567032212
Peer Küppers <peer.kueppers@h-ka.de>
2025-05-19 22:39:50 +0200

    initial

---
+++ README.md
@@ -0,0 +1,65 @@
+# Big data lecture - Labs
+## Fundamentals
+[Simple Analytics Pipeline](https://pkuep.github.io/pk-bigdata/fundamentals_simple_analytics_pipeline){:target="_blank" rel="noopener"}
+
+[Data Science Sandbox](https://pkuep.github.io/pk-bigdata/fundamentals_data_science_sandbox){:target="_blank" rel="noopener"}
+
+[Data Warehouse Example](https://pkuep.github.io/pk-bigdata/fundamentals_data_warehouse_example){:target="_blank" rel="noopener"}
+
+[Batch Processing](https://pkuep.github.io/pk-bigdata/fundamentals_batch_processing){:target="_blank" rel="noopener"}
+
+[HDFS](https://pkuep.github.io/pk-bigdata/fundamentals_hdfs){:target="_blank" rel="noopener"}
+
+[MapReduce](https://pkuep.github.io/pk-bigdata/fundamentals_mapreduce){:target="_blank" rel="noopener"}
+
+## Data Ingestion
+
+[Image to GCS with Cloud Functions](https://pkuep.github.io/pk-bigdata/batch_ingestion_cloudfunctionimage){:target="_blank" rel="noopener"}
+
+[RDBMS to GCS with Cloud Functions](https://pkuep.github.io/pk-bigdata/batch_ingestion_cloudfunctionrdbms){:target="_blank" rel="noopener"}
+
+[RDBMS to HDFS/GCS with sqoop](https://pkuep.github.io/pk-bigdata/batch_ingestion_sqoop){:target="_blank" rel="noopener"}
+
+[RDBMS to GCS (Avro/Parquet) with sqoop](https://pkuep.github.io/pk-bigdata/batch_ingestion_sqoop_avroparquet){:target="_blank" rel="noopener"}
+
+[RDBMS to GCS with sqoop and Hive](https://pkuep.github.io/pk-bigdata/batch_ingestion_sqoop_hive){:target="_blank" rel="noopener"}
+
+[Data Fusion](https://pkuep.github.io/pk-bigdata/batch_ingestion_datafusion){:target="_blank" rel="noopener"}
+
+[Data Fusion with BigQuery](https://pkuep.github.io/pk-bigdata/batch_ingestion_datafusion_bigquery){:target="_blank" rel="noopener"}
+
+
+
+## Batch Processing
+
+[PySpark Wordcount](https://pkuep.github.io/pk-bigdata/batch_processing_pyspark_wordcount){:target="_blank" rel="noopener"}
+
+[PySpark SQL, DataFrames, Machine Learning and BigQuery-Access](https://pkuep.github.io/pk-bigdata/batch_processing_pyspark_advanced){:target="_blank" rel="noopener"}
+
+[Dask on DataProc](https://pkuep.github.io/pk-bigdata/batch_processing_dask_on_dataproc){:target="_blank" rel="noopener"}
+
+[Dask with Kubernetes (not working anymore)](https://pkuep.github.io/pk-bigdata/batch_processing_dask){:target="_blank" rel="noopener"}
+
+
+## Querying and Data Integration
+
+[BigQuery access to the Data Lake](https://pkuep.github.io/pk-bigdata/query_and_extraction_bigquery_datalake_access){:target="_blank" rel="noopener"}
+
+[Spark SQL access to the Data Lake](https://pkuep.github.io/pk-bigdata/query_and_extraction_sparksql_datalake_access){:target="_blank" rel="noopener"}
+
+[RDBMS Integration with sqoop](https://pkuep.github.io/pk-bigdata/query_and_extraction_sqoop_integration_rdbms){:target="_blank" rel="noopener"}
+
+[RDBMS Integration with Data Fusion](https://pkuep.github.io/pk-bigdata/query_and_extraction_datafusion_integration_rdbms){:target="_blank" rel="noopener"}
+
+
+
+## Stream Processing
+
+[Using PubSub from Python and the Service Account Concept](https://pkuep.github.io/pk-bigdata/realtime_analytics_pubsub){:target="_blank" rel="noopener"}
+
+[PubSub to Cloud Storage with DataFlow](https://pkuep.github.io/pk-bigdata/realtime_analytics_dataflow_cloudstorage){:target="_blank" rel="noopener"}
+
+[PubSubLite Streaming Analytics with Spark Streaming](https://pkuep.github.io/pk-bigdata/realtime_analytics_pubsublite_spark_streaming){:target="_blank" rel="noopener"}
+
+[PubSub Analytics with SQL-based DataFlow (GCP product not available anymore)](https://pkuep.github.io/pk-bigdata/realtime_analytics_dataflow_sql){:target="_blank" rel="noopener"}
+
---
+++ update_labs.bat
@@ -0,0 +1,90 @@
+git pull
+
+REM Fundamentals
+REM ----------------------------------------------------------------------
+
+REM Big Data Course - Codelab - Fundamentals - Simple Analytics Pipeline
+claat export 1KTHQeixpsTLILCw672EbYtD_AJPKSPN6XkicQu8F_B4
+
+REM Big Data Course - Codelab - Fundamentals - Data Science Sandbox
+claat export 1u9epU5jnnc2cS9R-eBhbj1R_ngQ2KeCFGQRxECx9q8w
+
+REM Big Data Course - Codelab - Fundamentals - Data Warehouse Example
+claat export 1Jjfa2iUQRFVPpxfCxo5Esq1NNeR-j8r_i6aVHOjm0Og
+
+REM Big Data Course - Codelab - Fundamentals - Batch Processing
+claat export 1wZW04gr56uqVikSk6tG2tWD27ZXf-xFyCTxcbqnuDgw
+
+REM Big Data Course - Codelab - Fundamentals - HDFS
+claat export 1Q0fqp1dnnbNUnm4ung22cmW8GTkD4_XjDAZGTQ9n1qI
+
+REM Big Data Course - Codelab - Fundamentals - MapReduce
+claat export 1Ap90DCSzQGsGqhYR5ZrB1iYDfkKM4fDB0BCi0kUBRl0
+
+REM Big Data Course - Codelab - Batch Ingestion - Image to GCS with Cloud Functions
+claat export 1nBA4IRqpceoclmk3dHbl-6Tkn7UJDfToGIkOqhrA2B0
+
+REM Big Data Course - Codelab - Batch Ingestion - Ingest RDBMS Source with a Cloud Function
+claat export 11JzWbgDpQv6F3v7GkxllI7MH1jC9VYpnY9e8ilU9qFM
+
+REM Big Data Course - Codelab - Batch Ingestion - Ingest RDBMS Source with Sqoop
+claat export 10Cx9TeqzR-6gh6V5GgjEAsSKcfU1Ul6RreGuRjfpve8
+
+REM Big Data Course - Codelab - Batch Ingestion - Ingest RDBMS Source with Sqoop to Avro/Parquet
+claat export 1H0BWTlnXDt0vSi_MDbz88TlNsZPmzlFIu2V_wlK7rpw
+
+REM Big Data Course - Codelab - Batch Ingestion - Ingest RDBMS Source with Sqoop and Hive
+claat export 1tiEdT5orlrUD7NaZdYj1BSnAqd7WSr2U6iyePqqrb8o
+
+REM Big Data Course - Codelab - Batch Ingestion - Cloud Data Fusion
+claat export 1ny2NVO-eH0pzHVIgc6LAvHU0JWeCx_RWDVQ6QPlbL_g
+
+REM Big Data Course - Codelab - Batch Ingestion - Cloud Data Fusion (BigQuery)
+claat export 1Je_l8XFxMprwQBR4i10DXXcP6rXb0QamhkgsZyglQF4
+
+
+
+
+REM Big Data Course - Codelab - Batch Processing - PySpark Wordcount
+claat export 1lz_lisVkUx8GJzW1PmvzFvawhink4HGeoWJDpKLPLjM
+
+REM Big Data Course - Codelab - Batch Processing - PySpark Advanced
+claat export 1_DADcz2CB3DUreOl5QVLntrWWWR5Ju1kxavUI3KHru8
+
+REM Big Data Course - Codelab - Batch Processing - Dask with Kubernetes (NOT WORKING)
+claat export 1S0Weqb88t6PijfNa00AA3Pgo3hzYtUk3Qo6LLVC4n1w
+
+REM Big Data Course - Codelab - Batch Processing - Dask on DataProc
+claat export 1lB_cDlDQWTesYdRE6jUFncrJEYG_2hU2-yJlJo11ofc
+
+
+REM Big Data Course - Codelab - Query and Extraction - Data Lake Queries from BigQuery
+claat export 1gqtt_j4eRxW2kVYPG8OvQj10g0TT7ioCSghcujjCDcE
+
+REM Big Data Course - Codelab - Query and Extraction - Data Lake Queries from Spark SQL
+claat export 1D6TxCqGZCdqSBM_7gpXDygAbmgz7OlClUcVRythWtp0
+
+REM Big Data Course - Codelab - Query and Extraction - Extracting Data with sqoop
+claat export 1wO3g8TzgbTy7dC-W-RSOmYlThM-y6y9DI_HHhGE4v2g
+
+REM Big Data Course - Codelab - Query and Extraction - Extracting Data with Data Fusion
+claat export 1OUeFRWJZTaxrKPXFY6TsY_SU7UeOlGRBhfgrVqw2Nns
+
+
+REM Big Data Course - Codelab - Realtime Analytics - PubSub and Service Accounts
+claat export 1JkYs2spek0jN0iQ0ni7SW_fB8lmnYt8Qy9HvAfO797E
+
+REM Big Data Course - Codelab - Realtime Analytics - PubSub to Cloud Storage with DataFlow 
+claat export 1H5qCQ8fs-QEC6LRn0csY3doZ4SZihcYKG7fPhlbLpuE
+
+REM Big Data Course - Codelab - Realtime Analytics - PubSubLite Streaming Analysis with Spark Streaming 
+claat export 1C6RJAiBuhlh7sS4BBHUj9jOqYTCvXjDdCjRh2SZRdJQ
+
+REM Big Data Course - Codelab - Realtime Analytics - PubSub Data Aggregation with SQL and DataFlow (NOT WORKING)
+claat export 17O7Ejx9Lo0fvj_0JCergVFj_N0LrvtjBUm0AG3Fn13I
+
+git add *.*
+
+git commit -m "update of codelabs"
+
+git push
